d8275b83789f13ef5eeda2ebd21290e757c5a6fe
varanganti-j <varanganti.j@PROD.HCLPNP.COM>
2018-12-06 15:12:45 +0530

    Adding backend script files to repository.

---
+++ scripts/startup.sh
@@ -0,0 +1,4 @@
+sudo ./start-all-containers.sh
+
+cd $(pwd)/Jenkins/Jenkins-automation
+sudo ./startup.sh
---
+++ scripts/stop-all-containers.sh
@@ -0,0 +1,10 @@
+echo 'executing UrbancodeVelocity docker-compose stop'
+cd UrbanCodeVelocity
+sudo docker-compose stop
+
+echo 'executing UrbanCodeDeploy docker-compose stop'
+cd ../UrbanCodeDeploy
+sudo docker-compose stop
+
+
+
